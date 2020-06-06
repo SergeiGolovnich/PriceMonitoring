f2cc4d9618473c9b7e12f7ade81ed485ce2ea1f3
Sergei <sergei@SERGEI-LAPTOP>
2020-06-06 11:50:35 +0300

    Добавлен простой кэш

---
+++ SimpleCacheTests/LUCacheTests.cs
@@ -0,0 +1,50 @@
+﻿using Xunit;
+using SimpleLRUCache;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleLRUCache.Tests
+{
+    public class LUCacheTests
+    {
+        [Fact()]
+        public void LUCacheDefaultTest()
+        {
+            LUCache<string, object> cache = new LUCache<string, object>();
+
+            Assert.False(cache.Contains("Some key"), "Cache must be empty");
+        }
+
+        [Fact()]
+        public void CanGetObject()
+        {
+            LUCache<string, object> cache = new LUCache<string, object>();
+
+            object objExpected = new object();
+            cache.Add("1", objExpected);
+
+            object objActual = cache.Get("1");
+
+            Assert.True(objActual.Equals(objExpected), "Must be same object");
+        }
+
+        [Fact()]
+        public void AddTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void GetTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ContainsTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
---
+++ SimpleLRUCache/LUCache.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace SimpleLRUCache
+{/// <summary>
+/// Last Used Cache
+/// </summary>
+    public class LUCache<TKey, TValue>
+    {
+        private int capacity;
+        private Dictionary<TKey, TValue> dict;
+        private LinkedList<TKey> list;
+        public LUCache() : this(10)
+        {
+
+        }
+        public LUCache(int capacity)
+        {
+            this.capacity = capacity;
+
+            dict = new Dictionary<TKey, TValue>(capacity);
+
+            list = new LinkedList<TKey>();
+        }
+
+        public void Add(TKey key, TValue value)
+        {
+            if (Contains(key))
+            {
+                return;
+            }
+
+            if(list.Count == capacity)
+            {
+                RemoveLastUsed();
+            }
+
+            dict.Add(key, value);
+
+            list.AddFirst(key);
+        }
+
+        private void RemoveLastUsed()
+        {
+            dict.Remove(list.Last.Value);
+
+            list.RemoveLast();
+        }
+
+        public TValue Get(TKey key)
+        {
+            TValue value;
+
+            if(dict.TryGetValue(key, out value))
+            {
+                MoveKeyForward(key);
+
+                return value;
+            }
+            else
+            {
+                throw new Exception($"No such item: {key}");
+            }
+        }
+
+        private void MoveKeyForward(TKey key)
+        {
+            if(list.First.Value.Equals(key))
+            {
+                return;
+            }
+
+            list.Remove(key);
+            list.AddFirst(key);
+        }
+
+        public bool Contains(TKey key)
+        {
+            return dict.ContainsKey(key);
+        }
+    }
+}
